5e6fe0ddfde3d3a5eeb791ba9e4c1748731e0792
Alex P <afapenha@gmail.com>
2024-10-13 03:50:52 -0400

    idk what i changed here, hope it doesnt break

--- Labs/lab02/lab02/app.js
+++ Labs/lab02/lab02/app.js
@@ -1,22 +1,11 @@
-require("dotenv").config();
 const express = require("express");
-const app = express();
 const PORT = process.env.PORT || 8000;
-const path = require("path");
-const labRouter = require("./router")
- 
- 
-app.use("/lab2", labRouter)
+const app = express();
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "views", "home.html"));
+    res.send("Welcome to our server");
 });
- 
-app.use("", (req, res) => {
-  res.status(404).send("Page Not Found");
-});
- 
- 
+
 app.listen(PORT,() =>{
     console.log(`http://localhost:${PORT}`)
 });
