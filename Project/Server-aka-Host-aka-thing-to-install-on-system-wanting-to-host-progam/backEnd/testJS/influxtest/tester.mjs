15c04a74529eb3b207fc8282749018b47e90603f
Alex <125697456+BeaVert16@users.noreply.github.com>
2024-12-07 06:23:43 -0500

    Delete Project/Server-aka-Host-aka-thing-to-install-on-system-wanting-to-host-progam/backEnd/testJS directory

--- Project/Server-aka-Host-aka-thing-to-install-on-system-wanting-to-host-progam/backEnd/testJS/influxtest/tester.mjs
+++
@@ -1,9 +0,0 @@
-// const { createBucket, writeData } = require('./influxtest.js');
-
-import { createBucket, writeData } from "./influxtest.js";
-
-(async () => {
-  const bucketName = 'test_bucket';
-  await createBucket(bucketName);
-  await writeData(bucketName);
-})();
